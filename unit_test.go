65db613de05de48fc6960f2fa6a77f0fa2332fcf
ninza <ninzasingh44@gmail.com>
2025-06-03 22:51:54 +0530

    testing for add

--- unit_test.go
+++ unit_test.go
@@ -1,12 +1,35 @@
+// package math
+
+// import "testing"
+
+// func TestAdd(t *testing.T) {
+//   	sum := Add(4,6)
+// 	want :=10
+// 	 if sum != want{
+// 		t.Errorf("got %d, wanted %d",sum,want)
+// 	 }
+
+// }
+
+
 package math
 
 import "testing"
 
-func TestAdd(t *testing.T) {
-  	sum := Add(4,6)
-	want :=10
-	 if sum != want{
-		t.Errorf("got %d, wanted %d",sum,want)
-	 }
+type addTest struct{
+	frst_num, sec_num ,expected_res int
+}
+var addTests =[]addTest{
 
+	addTest{2,3,5},
+	addTest{4,8,12},
+	addTest{6,9,15},
+	addTest{3,10,13},
+}
+func TestAdd(t *testing.T){
+	 for _, test :=range addTests{
+		if output := Add(test.frst_num, test.sec_num); output != test.expected_res {
+			t.Errorf("output %q not equal to expected %q", output,test.expected_res)
+		}
+	 }
 }
